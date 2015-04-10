ff367ae443314963160b4959c2e626c0e85fa7d9
Lumucro <brentvh.97@live.nl>
2015-04-10 16:52:24 +0200

    updated rate of getting XP

--- lua/vloms/config.lua
+++ lua/vloms/config.lua
@@ -7,7 +7,7 @@ Vloms.Logging = false --Log events like players leaving etc
 Vloms.LoggingExtensive = false --More detailed logging
 
 Vloms.AutoXP = true --Automatically give XP every interval
-Vloms.AutoXPInterval = 1 --Interval in seconds
-Vloms.AutoXPAmount = 5 --XP to give every interval
+Vloms.AutoXPInterval = 5 --Interval in seconds
+Vloms.AutoXPAmount = 1 --XP to give every interval
 
 Vloms.PerksEnabled = true --Enable or disable perks
