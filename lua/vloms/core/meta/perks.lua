2a1e39e3617affef06049803766fe645ef145afe
Lumucro <brentvh.97@live.nl>
2015-04-10 23:25:35 +0200

    Fixed a stupid mistake that'd stop adding perks if you already had one

--- lua/vloms/core/meta/perks.lua
+++ lua/vloms/core/meta/perks.lua
@@ -2,15 +2,12 @@ local pm = FindMetaTable( 'Player' )
 
 function pm:VFetchPerks()
 
+	if (self.vlomsperks == nil) then self.vlomsperks = {} end --Safety
+
 	for i=1,#Vloms.Perks do
 		
-		if (self.vlomsperks == nil) then self.vlomsperks = {} end --Safety
-		if (table.HasValue(self.vlomsperks, i)) then return end --Already has this perk loaded
-
-		if (tonumber(Vloms.Perks[i]['lvl']) <= tonumber(self.vlomslvl)) then
-			
+		if (!table.HasValue(self.vlomsperks, i) && (tonumber(Vloms.Perks[i]['lvl']) <= tonumber(self.vlomslvl))) then
 			self.vlomsperks[#self.vlomsperks+1] = i
-
 		end
 
 	end
